73c648e92ec7f8c659bde64823fb817f7ab05431
Ramses Gasque <cedro.gasque@wsu.edu>
2020-03-06 11:59:15 -0800

    a

--- pages/insights.py
+++ pages/insights.py
@@ -5,6 +5,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+import eli5
+from eli5.sklearn import PermutationImportance
+
+from joblib import load
+permuter = load('assets/permuter.pkl')
+print('Pipeline loaded!')
+feature_names = ['duration', 'tower_status_radiant', 'tower_status_dire', 'barracks_status_radiant', 'barracks_status_dire', 'first_blood_time']
 # Imports from this application
 from app import app
 
@@ -16,11 +23,19 @@ column1 = dbc.Col(
             """
         
             ## Insights
-
-
+			
+			I ended up with an absurdly high accuracy for this model,
+			but that's mostly due to the data points I ended up picking.
+			
+			I ended up choosing first blood time (when the first kill happened), duration of the game,
+			the "status" of both teams' barracks and both teams' towers
+			(I have no idea what these mean, so this project was 95% a bust).
+			
+			Here you can see the permutation importances:
             """
         ),
-
+		html.Img(src='../assets/permutation_importances.png', className='img-fluid'),
+		
     ],
 )
 
--- pages/process.py
+++ pages/process.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+from textwrap import dedent
 # Imports from this application
 from app import app
 
@@ -12,14 +12,44 @@ from app import app
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
-        dcc.Markdown(
-            """
+        dcc.Markdown(dedent("""
         
-            ## Process
+## Process
 
+The first step was getting the data into a manageable form,
+as my original data set was way too large,
+with over 8 GB total data.
 
+I ended up losing a lot of interesting information due to
+not being able to actually matchup up any of the data with each other,
+as I think the samples I used weren't actually part of a whole.
+
+In the end, I just ended up choosing every
+game with 10 players I had,
+and measuring the six classes mentioned in Insights.
+
+I used a pipeline with scikit-learn's SimpleImputer
+and StandardScaler, followed by XGBoost's XGBClassifier.
+
+```python
+
+pipeline = make_pipeline(
+    SimpleImputer(),
+    StandardScaler(),
+    XGBClassifier(max_depth=10,
+                  learning_rate=0.2,
+                  booster='gbtree',
+                  gamma=0.01,
+                  n_jobs=-1,
+                  random_state=0,
+                  early_stopping_rounds=50,
+                  verbosity=3),
+    verbose=True
+)
+
+```
             """
-        ),
+        )),
 
     ],
 )
